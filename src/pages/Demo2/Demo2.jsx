a6259ebbb4c66e2da8f20701b2a3ece5e298ac99
zxymax <497084653@qq.com>
2022-03-28 10:38:28 +0800

    Formik Field Form ErrorMessage

--- src/pages/Demo2/Demo2.jsx
+++ src/pages/Demo2/Demo2.jsx
@@ -1,4 +1,5 @@
-import { useFormik } from 'formik'
+//import { useFormik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import './style.css'
 import * as Yup from 'yup'
 
@@ -10,8 +11,16 @@ import * as Yup from 'yup'
   }
 
   const onSubmit = (values) => {
-
+    console.log('form data',values)
   }
+
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email format').required('Required'),
+    channel: Yup.string().required('Required')
+  })
+
 
 /*
 
@@ -37,54 +46,65 @@ import * as Yup from 'yup'
 
 */
 
-  const validationSchema = Yup.object({
-    username: Yup.string().required('Required'),
-    email: Yup.string().email('Invalid email format').required('Required'),
-    channel: Yup.string().required('Required')
-  })
 
 
 
 
-
-function OldYoutubeFormik() {
-
+const YoutubeFormik =() =>  {
 
 
+  
+
+  /*
   const formik = useFormik({
     initialValues,
     onSubmit,
  //   validate
     validationSchema
   })
+  */
 
   return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      >
+    {/*<Form onSubmit={formik.handleSubmit}>*/}
+
     <>
+
       <h3>Youtube Formik</h3>
-      <form onSubmit={formik.handleSubmit}>
+      <Form>
       <div>
       <label htmlFor="username">Username</label>
-      <input type="text" id="username" name="username" {...formik.getFieldProps('username')} />
-      {formik.touched.username && formik.errors.username?<p className="error">{formik.errors.username}</p>:null}
+    {/*<input type="text" id="username" name="username" {...formik.getFieldProps('username')} />*/}
+      <Field type="text" id="username" name="username" />
+      {/*formik.touched.username && formik.errors.username?<p className="error">{formik.errors.username}</p>:null */}
+      <ErrorMessage name="username" />
       </div>
       <div>
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" {...formik.getFieldProps('email')} />
-      {formik.touched.email && formik.errors.email?<p className="error">{formik.errors.email}</p>:null}
-    </div>
+    {/*<input type="email" id="email" name="email" {...formik.getFieldProps('email')} />*/}
+      <Field type="email" id="email" name="email"  />
+      {/*formik.touched.email && formik.errors.email?<p className="error">{formik.errors.email}</p>:null */}
+      <ErrorMessage name="email" />
+      </div>
 
       <div>
       <label htmlFor="channel">Channel</label>
-      <input type="channel" id="channel" name="channel" {...formik.getFieldProps('channel')} />
-      {formik.touched.channel && formik.errors.channel?<p className="error">{formik.errors.channel}</p>:null}
+    {/*<input type="channel" id="channel" name="channel" {...formik.getFieldProps('channel')} />*/}
+      <Field type="channel" id="channel" name="channel" />
+      {/* formik.touched.channel && formik.errors.channel?<p className="error">{formik.errors.channel}</p>:null */}
+      <ErrorMessage name="channel" />
     </div>
 
       <button type="submit">Submit</button>
-    </form>
+    </Form>
     </>
+    </Formik>
   )
 }
 
-export default OldYoutubeFormik
-
+export default YoutubeFormik
 
